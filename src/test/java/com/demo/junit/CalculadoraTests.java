8905af92c4848d6147824cac4ad066d6527825a7
Jonathan Buzzetti <jonathan.buzzetti@naranjax.com>
2022-05-04 00:48:25 -0300

    agrego test vacío

---
+++ src/test/java/com/demo/junit/CalculadoraTests.java
@@ -0,0 +1,13 @@
+package com.demo.junit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTests {
+
+    @Test
+    public void test(){
+        Assertions.assertTrue(true);
+    }
+
+}
